6f995984cf6bcfb07974faf70327ab48b1d683f6
blackmau5 <sergiu.liogchii@gmail.com>
2019-09-07 18:40:43 +0300

    Added cs50 solution

--- validtriangle.c
+++ validtriangle.c
@@ -23,11 +23,30 @@ bool valid_triangle(float a, float b, float c)
 {
     while (a > 0 && b > 0 && c > 0)
     {
-        if (a + b > c && a + c > b && b + c > a) 
+        if ((a + b > c) && (a + c > b) && (b + c > a)) 
         {
             return true; 
         }
         return false;
     }
     return false;
+}
+
+//cs50 solution
+bool valid_triangle(float x, float y, float z)
+{
+    //check for all positive sides
+    if (x <= 0 || y <= 0 || z <= 0)
+    {
+        return false;
+    }
+    
+    //check that sum of any two sides is greater than third
+    if ((x + y <= z) || (x + z <= y) || (y + z <= x))
+    {
+        return false;
+    }
+    
+    //if we passed both tests, we're good!
+    return true;
 }
